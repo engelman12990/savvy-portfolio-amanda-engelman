ac13b587f91b44c7c14157a12df3f08b9375bd13
Amanda Engelman <engelman12990@gmail.com>
2019-02-18 19:09:31 -0600

    Added recursions

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
-var nameChecker = function(){
-    var name = prompt("Please enter your name:");
-    if (name === "") {
-      var name = prompt("For real - enter your name:");
-    } else {
-    document.querySelector("h1").textContent = `Hello ${name}`;
-   }
+// Put 'name' in the global scope so it is reliably accessible by both functions.
+var name;
 
-   nameChecker();
+// Verify that user entered some name
+var blankChecker = function blankChecker(){ // We should also add an explicit name in our fxn. declaration as that will help us with tracing possible errors.
+    if(name === ''){
+        name = prompt('For real, enter your name!');
+
+        // RECURSION
+        blankChecker();
+    }
+};
+
+// 'Main' fxn. to start the program
+var nameChecker = function nameChecker(){
+    name = prompt('Please enter your name!');
+
+    // Check for a blank name!
+    blankChecker();
+
+    document.querySelector('h1').textContent = `Hello, ${name}`;
+};
+
+nameChecker();
