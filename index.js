026d7f632d89893c11f2d3a91e0d686cc9c66c87
Amanda Engelman <engelman12990@gmail.com>
2019-02-28 21:09:35 -0600

    Refactor nav links

--- index.js
+++ index.js
@@ -1,68 +1,58 @@
-import Navigation from './src/Navigation';
-import Header from './src/Header';
 import Content from './src/Content';
 import Footer from './src/Footer';
-// import nameChecker from './src/Greeter';
+import Header from './src/Header';
+import Navigation from './src/Navigation';
 
-
-// Create a STATE object
-
-var root = document.querySelector('#root');
 var State = {
-    'Home': {
-        'title': 'Welcome to Amanda\'s Savvy Coders Website'
-    },
     'Blog': {
-        'title': 'Read My Junk'
+        'links': [ 'Home', 'Contact', 'Projects' ] ,
+        'title': 'Welcome to my Blog'
+    },
+    'Home': {
+        'links': [ 'Blog', 'Contact', 'Projects' ] ,
+        'title': 'Welcome to my Portfolio'
     },
     'Contact': {
+        'links': [ 'Home', 'Blog', 'Projects' ] ,
         'title': 'Contact Me'
     },
     'Projects': {
-        'title': 'Check My Project'
+        'links': [ 'Home', 'Blog', 'Contact' ] ,
+        'title': 'Check out my Projects'
     }
 };
 
+var root = document.querySelector('#root');
+var render;
 
-function render(state){
-    var links;
+function navHandler(event){
+    event.preventDefault();
 
-    root.innerHTML = `
-    ${Navigation(state)}
-    ${Header(state.title)}
-    ${Content(state)}
-    ${Footer(state)}
-    `;
-
-    links = document.querySelectorAll('#navigation>ul>li>a');
-
-    links[0].addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        render(State[event.target.textContent]);
-    });
-
-    links[1].addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        render(State[event.target.textContent]);
-    });
-
-    links[2].addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        render(State[event.target.textContent]);
-    });
-    links[3].addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        render(State[event.target.textContent]);
-    });
+    render(State[event.target.textContent]); // WE must use BRACKET NOTATION to access something in an Object dynamically.
 }
 
+
+// This grabs our state and passes into to render each page
+render = function Render(state){
+    var links;
+    var i = 0;
+
+    root.innerHTML = `
+        ${Navigation(state)}
+        ${Header(state.title)}
+        ${Content(state)}
+        ${Footer(state)}
+    `;
+
+    links = document.querySelectorAll('#navigation > ul > li > a');
+
+    // Run a while loop for as long as the LENGTH of links is.
+    while(i < links.length){
+        // Change the index
+        links[i].addEventListener('click', navHandler);
+
+        i++;
+    }
+};
+
 render(State.Home);
-
-
-// nameChecker();
-
-    
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,25 @@
-export default function Navigation(){
-  return `  
-<div id="navigation">
-<ul class="container">
-  <li><a href="#">Home</a></li>
-  <li><a href="./blog/">Blog</a></li>
-  <li><a href="./contact/">Contact</a></li>
-  <li><a href="./projects/">Projects</a>
-    <ul>
-      <li><a href=""></a>First</li>
-      <li><a href=""></a>Second</li>
-      <li><a href=""></a>Third</li>
-    </ul>
-</ul>
-</li>
-</ul>
-</div>
-`;
+function linkBuilder(links){
+    var i = 0;
+    var linksHTML = '';
+
+    while(i < links.length){
+        linksHTML += `
+     <li>
+        <a href='./${links[i]}'>${links[i]}</a>
+     </li>
+  `;
+        i++;
+    }
+    
+    return linksHTML;
+}
+
+export default function Navigation(state){
+    return `  
+    <div id="navigation">
+      <ul class="container">
+        ${linkBuilder(state.links)}
+      </ul>
+    </div>
+  `;
 }
